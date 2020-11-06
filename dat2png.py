9cd3332b903ce4f42955fd1316bd04df6fe0c011
nagayosi <koutarou.naga44@icloud.com>
2020-11-07 06:18:37 +0900

    Update

--- dat2png.py
+++ dat2png.py
@@ -9,6 +9,9 @@ import matplotlib.pylab as plt
 import cv2
 import sys
 import os
+import random
+
+from PIL import Image
 
 def scale(x,s=[0,7],t=[0,1]):
     d_t = t[0]-t[1]
@@ -16,8 +19,7 @@ def scale(x,s=[0,7],t=[0,1]):
     ratio = (d_t/d_s)
     d_s_t = t[0] - s[0]*ratio
     # s range -> t range
-    res = x*ratio+d_s_t
-
+    res = x*ratio+d_s_ta
     return res
 
 def cmap(x,sta=[255,0,56],end=[0,0,255]): #x:scalar , sta,end:[B,G,R]
@@ -30,20 +32,22 @@ def ll2ij(point,starts,dist): # point=[lon,lat] , starts=[lon_st,lat_st] , dist=
     j = (point[1]-starts[1])/dist[1]
     return [i,j]
     
-
-
 # datファイルの一覧を取得
 path = "data/SeismicCoefficient/"
 files = sorted(glob.glob(path+"2013_Sv05s_LL/*.dat"))
 
+rad = float(sys.argv[1])  # pixel
+isLog = "log" in sys.argv[2] # scaling by log
+
 df = pd.read_csv(files[0],names=["lon", "lat", "amp", "sid"])
 lons_df = np.unique(df['lon'].values)
 lats_df = np.unique(df['lat'].values)[::-1]
+p_df = np.array([df['lon'].values,df['lat'].values]).transpose() # [M,2] 全データ
 
 #maps = pickle.load(open(path+"maps_amp.pickle"))
-
 excel = pd.read_excel(path+"site_schema_20200322A.xlsx")
 
+# 観測点
 # exist points
 p_ex = np.array([excel['lon'].values,excel['lat'].values]) 
 
@@ -51,12 +55,12 @@ p_ex = np.array([excel['lon'].values,excel['lat'].values])
 dis_lon = lons_df[1]-lons_df[0]
 dis_lat = lats_df[0]-lats_df[1]
 
+# 観測値のxy軸の端
 # start and end points of simulation
 st = np.array([np.min(lons_df),np.min(lats_df)])
 en = [np.max(lons_df),np.max(lats_df)]
 
 # cut points over simulation range
-
 keep_ind = []
 keep_lons = np.concatenate([np.where(p_ex[0]>=st[0]-dis_lon/2)[0],np.where(p_ex[0]<=en[0]+dis_lon/2)[0]])
 uni,counts_lon = np.unique(keep_lons,return_counts=True)
@@ -65,85 +69,135 @@ keep_ind.append(uni[counts_lon>=2])
 keep_lats = np.concatenate([np.where(p_ex[1]>=st[1]-dis_lat/2)[0],np.where(p_ex[1]<=en[1]+dis_lat/2)[0]])
 uni,counts_lat = np.unique(keep_lats,return_counts=True)
 keep_ind.append(uni[counts_lat>=2])
-
-keep_ind = np.unique(np.concatenate(keep_ind))
-
-p_ex = p_ex[:,keep_ind].transpose() #[N,2]
-
-
-p_df = np.array([df['lon'].values,df['lat'].values]).transpose() # [M,2]
-
-rad = float(sys.argv[1])  # pixel
+uni,counts = np.unique(np.concatenate(keep_ind),return_counts=True)
+keep_ind = uni[counts>=2]
 
 #pdb.set_trace()
-# scaling [lon_s~lon_e,lat_s~lat_e] -> [0~507,0~517]
-xy_df = (p_df - np.tile(st[np.newaxis], (p_df.shape[0],1)))/np.array([dis_lon,dis_lat])
-xy_ex = (p_ex - np.tile(st[np.newaxis], (p_ex.shape[0],1)))/np.array([dis_lon,dis_lat])
+p_ex = p_ex[:,keep_ind].transpose() #[N,2] 観測所
 
-n = xy_ex.shape[0]
-m = xy_df.shape[0]
+#======================================================================
+#pdb.set_trace()
+if rad > 0:
+    # scaling [lon_s~lon_e,lat_s~lat_e] -> [0~507,0~517]
+    xy_df = (p_df - np.tile(st[np.newaxis], (p_df.shape[0],1))) / np.array([dis_lon,dis_lat])
+    xy_ex = (p_ex - np.tile(st[np.newaxis], (p_ex.shape[0],1))) / np.array([dis_lon,dis_lat])
 
-# calculate distances
-dists = np.tile(xy_df,(n,1)) - np.reshape(np.tile(xy_ex,(1,m)),[n*m,2])
-dists = np.linalg.norm(dists, ord=2, axis=1)
+    # 観測点から半径rad の円に中心が含まれるピクセルを観測ピクセルとして採用
+    n = xy_ex.shape[0]
+    m = xy_df.shape[0]
 
-p_ex = p_df[np.unique( np.where(dists<=rad)[0] % m )]
-
+    # calculate distances
+    dists = np.tile(xy_df,(n,1)) - np.reshape(np.tile(xy_ex,(1,m)),[n*m,2])
+    dists = np.linalg.norm(dists, ord=2, axis=1)
+    p_ex = p_df[np.unique( np.where(dists<=rad)[0] % m )]
 
 # points lon,lat ->  indexs i,j
+#pdb.set_trace()
 inds_ex = []
 for p in p_ex:
     i = np.abs(lons_df-p[0]).argmin()
     j = np.abs(lats_df-p[1]).argmin()
     inds_ex.append([i,j])
-#pdb.set_trace()
 inds_ex = np.array(inds_ex).transpose()
 
-"""
-# expand inds_ex (L1)
-new_inds = []
-rad = int(sys.argv[1])
-
-for p in inds_ex:
-    for rx in range(rad+1):
-        for ry in range(rad+1):
-            if rx+ry <= rad:
-                new_inds.append(np.array([p[0]+rx,p[1]+ry]))
-                new_inds.append(np.array([p[0]-rx,p[1]+ry]))
-                new_inds.append(np.array([p[0]-rx,p[1]-ry]))
-                new_inds.append(np.array([p[0]+rx,p[1]-ry]))
-"""
-#inds_ex = np.unique(new_inds,axis=0).transpose()
-
-
-# make mask image
-mask = np.ones([len(lats_df),len(lons_df),3])*255
-
+#======================================================================
+# make mask image and sea image
+mask = np.zeros([len(lats_df),len(lons_df)])
+sea = np.zeros([len(lats_df),len(lons_df)])
 count = 0
 for ind in range(len(df)):
-    #pdb.set_trace()
     # 緯度と経度の座標取得
-    lonInd = int(np.where(df['lon'][ind]==lons_df)[0])
-    latInd = int(np.where(df['lat'][ind]==lats_df)[0])
+    lonInd = int(np.where(df['lon'][ind]==lons_df)[0]) #経度
+    latInd = int(np.where(df['lat'][ind]==lats_df)[0]) #緯度
 
-    if not (latInd in inds_ex[1][np.where(inds_ex[0]==lonInd)[0]]): # not Exist
+    if latInd in inds_ex[1][np.where(inds_ex[0]==lonInd)[0]]: # Exist
         count += 1
-        mask[latInd,lonInd] = np.array([0,0,0])
-pdb.set_trace()
-mask = mask.astype("uint8")
-cv2.imwrite(path+"mask_L2-rad{}.png".format(rad),mask)
+        mask[latInd,lonInd] = 255
+    sea[latInd,lonInd] = 1
 
-SAVE_DIR = path + "amp_map"+os.sep
-if not os.path.isdir(SAVE_DIR):
-    os.makedirs(SAVE_DIR)
+"""
+# クリギングは画像サイズ518x508で行う
+krigeMask = (mask*sea).astype("uint8")
+cv2.imwrite(path+"KrigingMask.png", krigeMask)
+cv2.imwrite(path+"KrigingSea.png", (sea*255).astype("uint8"))
+"""
+
+mask = mask.astype("uint8")
+mask = cv2.resize(mask,(512,512))
+_, mask = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)
+
+tmp_sea = cv2.threshold(cv2.resize(sea*255,(512,512)), 127, 255, cv2.THRESH_BINARY)[1]
+
+mask[tmp_sea==0] = 255
+
+#pdb.set_trace()
+
+# datasetPath
+d_path = os.path.join(path,"dataSet-r{}".format(rad))
+if isLog:
+    d_path += "-log"
+
+TRAIN_DIR = d_path+os.sep+"train"+os.sep+"train_amp"+os.sep
+VALID_DIR = d_path+os.sep+"valid"+os.sep+"valid_amp"+os.sep
+TEST_DIR = d_path+os.sep+"test"+os.sep+"test_amp"+os.sep
+
+TRAIN_MASK = d_path+os.sep+"train_mask"+os.sep
+VALID_MASK = d_path+os.sep+"valid_mask"+os.sep
+TEST_MASK = d_path+os.sep+"test_mask"+os.sep
+
+TRAIN_SEA = d_path+os.sep+"train_sea"+os.sep
+VALID_SEA = d_path+os.sep+"valid_sea"+os.sep
+TEST_SEA = d_path+os.sep+"test_sea"+os.sep
+
+for DIR in [d_path,TRAIN_DIR,VALID_DIR,TEST_DIR,TRAIN_MASK,VALID_MASK,TEST_MASK,TRAIN_SEA,VALID_SEA,TEST_SEA]:
+    if not os.path.isdir(DIR):
+        os.makedirs(DIR)
+
+# save sea and mask
+cv2.imwrite(path+"sea.png",tmp_sea)
+cv2.imwrite(path + 'mask_r{}.png'.format(rad),mask)
+pdb.set_trace()
 
 # make amp-map images
 maps = []
+seas = []
 names = []
+mask_names = []
+sea_names = []
+
+# data config file
+#pdb.set_trace()
+dcf_path = path + "DataConfig.txt" 
+datanum = len(files)
+validnum = datanum//10
+testnum = (datanum-validnum)//9
+trainnum = datanum-validnum-testnum
+
+if not os.path.isfile(dcf_path):
+    print("make new-dataset")
+    random.shuffle(files)
+    with open(dcf_path, mode='w') as f:
+        for ite,name in enumerate(files):
+            if ite < validnum: #valid
+                f.write(name+" valid\n")
+            elif validnum <= ite and ite < validnum+testnum: #test
+                f.write(name+" test\n")
+            else: # train
+                f.write(name+" train\n")
+    f.close()
+
+# データ分割情報をロード
+#pdb.set_trace()
+with open(dcf_path) as f:
+    l_strip = [s.strip() for s in f.readlines()] # remove '\n'
+    files = [st.split(" ")[0] for st in l_strip]
+    dtypes = [st.split(" ")[1] for st in l_strip]
+f.close()
+
+print("train:{}, valid:{}, test:{}".format(trainnum,validnum,testnum))
 # datファイルのループ
-for file in files:
-    print("loading {}".format(file))
-    #pdb.set_trace()
+for ite,file,dtype in zip(range(len(files)),files,dtypes):
+    print(" \r ite:{}".format(ite),end="")
 
     # datファイルの読み込み
     df = pd.read_csv(file,names=["lon", "lat", "amp", "sid"])
@@ -153,46 +207,76 @@ for file in files:
     lats = np.unique(df['lat'].values)[::-1]
 
     # マップ画像の初期化
-    map = np.ones([len(lats),len(lons),3])*255
+    map = np.zeros([len(lats),len(lons)])
+    sea = np.zeros([len(lats),len(lons)])
     #pdb.set_trace()
     #count = 0
-    
+    """
     # マップ画像の座標のループ（もっと賢いやり方がないのか？）
     for ind in range(len(df)):
         #pdb.set_trace()
         # 緯度と経度の座標取得
         lonInd = int(np.where(df['lon'][ind]==lons)[0])
         latInd = int(np.where(df['lat'][ind]==lats)[0])
-        """
-        if latInd in inds_ex[1][np.where(inds_ex[0]==lonInd)[0]]: # Exist
-            # 座標のピクセルに振幅を設定
-            map[latInd,lonInd] = cmap(df['amp'][ind])
-        else:
-            count += 1
-            map[latInd,lonInd] = np.array([0,0,0])
-        """
-        map[latInd,lonInd] = cmap(df['amp'][ind])
 
+        map[latInd,lonInd] = df['amp'][ind]
+        sea[latInd,lonInd] = 1
 
-    #print("count:{}".format(count))
-    map = map.astype("uint8")
-    maps.append(map)
-    names.append((file.split('.')[0]).split('/')[-1])
-
-    
-    # ヒートマップでマップ画像を表示
-    #xticklabels=[round(lons[i],2) if i%100==0 else '' for i in range(len(lons))]
-    #yticklabels=[round(lats[i],2) if i%100==0 else '' for i in range(len(lats))]
-    #sns.heatmap(map,xticklabels=xticklabels,yticklabels=yticklabels,vmin=0,vmax=7)
-    
-    #pdb.set_trace()
+    """
     fname = (file.split('.')[0]).split('/')[-1]
-    #plt.savefig(f"{fname}.png")
-    #plt.close()
-    #pdb.set_trace()
-    cv2.imwrite(SAVE_DIR+fname+".png",map)
+    maps.append(map)
+    seas.append(sea)
+    
 
-#pickle.dump([np.array(maps),np.array(masks),names],open(SAVE_DIR+"maps_amp_L1-rad{}.pickle".format(rad),"wb"))
-#pickle.dump(names,open(SAVE_DIR+"names_L1-rad{}.pickle".format(rad),"wb"))
+    if dtype=="valid": #valid
+        DIR = VALID_DIR
+        MASK = VALID_MASK
+        SEA = VALID_SEA
+    elif dtype=="test": #test
+        DIR = TEST_DIR
+        MASK = TEST_MASK
+        SEA = TEST_SEA
+    elif dtype=="train": # train
+        DIR = TRAIN_DIR
+        MASK = TRAIN_MASK
+        SEA = TRAIN_SEA
+    else:
+        print("Unknown value")
+        pdb.set_trace()
+
+    names.append(DIR+fname+".png")
+    mask_names.append(MASK+fname+".png")
+    sea_names.append(SEA+fname+".png")
+"""
+#pdb.set_trace()
+maps = np.array(maps)
+seas = np.array(seas)
+
+pdb.set_trace()
+
+# scaling
+scale_max = 8.0
+#scale_max = np.max(maps) # max=37.2756
+maps[maps>scale_max] = scale_max
+
+if isLog:
+    maps = np.log2((maps/scale_max)+1)*255
+    print("log-scaling")
+else:
+    maps = (maps/scale_max)*255
+    print("normal scaling")
+"""
+
+for map,sea,name,m_name,s_name in zip(maps,seas,names,mask_names,sea_names):
+    #map = map.astype("uint8")
+    #map = cv2.resize(map,(512,512))
+    #cv2.imwrite(name,map)
+    #sea = sea.astype("uint8")
+    #sea = cv2.resize(sea,(512,512))
+    #cv2.imwrite(s_name,sea)
+    mask = np.array(Image.open("./data/SeismicCoefficient/mask_r0.0.png"))
+    cv2.imwrite(m_name,mask)
+#pdb.set_trace()
+
 
 
---
+++ test_pyKriging.py
@@ -0,0 +1,187 @@
+import numpy as np
+import pdb
+import os
+import glob
+import pickle
+import time
+import cv2
+import sys
+import cupy as cp
+
+import pyKriging
+from pyKriging.krige import kriging  
+from pyKriging.samplingplan import samplingplan
+
+from PIL import Image
+
+
+def rangeError(pre,tru,domain=[-1.0,0.0],opt="MA"): # pred:予測値, true:真値 , domain:変域(domain[0]< x <=domain[1])
+    # normalyse
+    domain = [domain[0]/8,domain[1]/8]
+
+    inds = np.where(np.logical_and(tru>domain[0],tru<=domain[1]))
+
+    if inds[0].shape[0]==0:
+        return np.NaN
+
+    error_ = tru[inds[0],inds[1]]-pre[inds[0],inds[1]]
+    if opt=="MA": # MAE
+        error_ = np.mean(np.abs(error_))
+    elif opt=="MS": # MSE
+        error_ = np.mean(error_**2)
+    elif opt=="A":
+        error_ = np.abs(error_)
+
+    return error_
+
+
+class CuPyPredicter:
+    def __init__(self,model):
+        # Numpy -> Cupy
+        #pdb.set_trace()
+        self.n = cp.asarray(model.n)
+        self.mu = cp.asarray(model.mu)
+        self.theta = cp.asarray(model.theta)
+        self.pl = cp.asarray(model.pl)
+        self.U = cp.asarray(model.U)
+        self.one = cp.asarray(model.one)
+        self.y = cp.asarray(model.y) # [n]
+        self.X = cp.asarray(model.X) # [n,2]
+        self.normRange = cp.asarray(model.normRange)
+        self.ynormRange = cp.asarray(model.ynormRange)
+
+    def predict_normalized(self,x): # x:shape=[2]
+        Psi=cp.exp(-cp.sum(self.theta*cp.power((cp.abs(self.X-x[cp.newaxis,:])),self.pl)))
+        z = self.y-self.one.dot(self.mu)
+        a = cp.linalg.solve(self.U.T, z)
+        b = cp.linalg.solve(self.U, a)
+        c = Psi.T.dot(b)
+
+        f=self.mu + c
+        return f[0]
+
+    def predict(self,X): # X:shape=[2] 出力はNumpy
+        X = self.normX(cp.asarray(X))
+        return cp.asnumpy(self.inversenormy(self.predict_normalized(X)))
+
+    def normX(self, X):
+        '''
+        :param X: An array of points (self.k long) in physical world units
+        :return X: An array normed to our model range of [0,1] for each dimension
+        '''
+        return cp.array( (X - self.normRange[0][0]) / float(self.normRange[0][1] - self.normRange[0][0]) )
+
+    def inversenormy(self, y):
+        '''
+        :param y: A normalized array of model units in the range of [0,1]
+        :return: An array of observed values in real-world units
+        '''
+        return (y * (self.ynormRange[1] - self.ynormRange[0])) + self.ynormRange[0]
+
+
+
+if __name__ == "__main__":
+    
+    # GPU上での処理が終わるのを待ってから次の行を実行
+    cp.cuda.Stream.null.synchronize()
+
+    #pdb.set_trace()
+    # data dimension
+    dim = 2
+    samplePath = "data"+os.sep+"test_sample"
+    dataPath = "data"+os.sep+"SeismicCoefficient"
+    
+    pred_num = 6
+    exi = int(sys.argv[1]) # 実験番号
+
+    testData = pickle.load(open("data/raw_testData.pickle","rb"))
+    testImg = testData["amp"]
+    # 一部を実験
+    if pred_num*(exi+1)>testImg.shape[0]:
+        testImg = testImg[pred_num*exi:]
+    else:
+        testImg = testImg[pred_num*exi:pred_num*(exi+1)]
+    testImg[testImg>8] = 8
+    testImg = testImg/8
+    testNames = testData["name"]
+    maskImg = np.array(Image.open(os.path.join(dataPath,"KrigingMask.png")))
+    seaImg = np.array(Image.open(os.path.join(dataPath,"KrigingSea.png")))
+    inds = np.where(maskImg*seaImg>0)
+    inds = [inds[0][np.newaxis],inds[1][np.newaxis]]
+
+    X = np.concatenate(inds,axis=0).T #入力点 shape=[N,2]
+    
+    # 補間したい点を集める
+    target_mask = seaImg
+    tPoint = np.where(target_mask>0) # 補間したい点
+    tPoint = np.concatenate([tPoint[0][np.newaxis],tPoint[1][np.newaxis]]).T # shape=[M,2]
+    pnum = int(np.sum(target_mask/255)) # 1枚の画像内にある予測しないといけない点の数
+    
+    startFlag = True
+
+    pred_imgs= []
+    
+    # 範囲ごとのMAEを保存するリスト
+    maes, mae0, mae05_2, maes_sep = [],[],[],[]
+    for ite,img in enumerate(testImg):
+        print("{} of {} images".format(ite+1,testImg.shape[0]))
+        y = img[maskImg*seaImg>0]
+        
+        stime = time.time()
+        k = kriging(X, y, name='InpaintingKriging')
+        k.train()
+        train_time = time.time() - stime
+
+        Prediction = CuPyPredicter(k)
+
+        def predict(xs):
+            res = np.zeros_like(img)
+            for j,p in enumerate(xs):
+                print("\r predict ite:{}-point:{} ".format(ite+1,j),end="")
+                res[p[0],p[1]] = Prediction.predict([p[0],p[1]])
+            return res
+
+        # predict
+        stime = time.time()
+        pred = predict(tPoint)
+        print("{}sec for training, {}sec for predict".format(train_time,time.time()-stime))
+        pred_imgs.append(pred)
+
+        # 補間結果をプロットして保存
+        tmp = pred*np.ones_like(pred) # 参照渡しを防ぐ
+        tmp[tmp>1] = 1
+        tmp[tmp<0] = 0
+        testname = testNames[ite].split("/")[-1]
+        cv2.imwrite(os.path.join(samplePath,testname),tmp*255)
+    
+        # 各震度値ごとのMAE(テキスト) 
+        #pdb.set_trace()
+        mae = np.sum(np.abs(img-pred))/pnum
+        e0 = rangeError(pred,img,domain=[-1.0,0.0])
+        e05_2 = rangeError(pred,img,domain=[0.5,2.0])
+        sep_errs = [rangeError(pred,img,domain=[i*0.8,(i+1)*0.8]) for i in range(10)]
+
+        maes.append(mae)
+        mae0.append(e0)
+        mae05_2.append(e05_2)
+        maes_sep.append(sep_errs)
+
+    #pdb.set_trace()
+    summary_data = {
+        "pred":np.array(pred_imgs),
+        "name":testNames,
+        "MAE":np.mean(np.array(maes)),
+        "MAE0":np.mean(np.array(mae0)),
+        "MAE-sep0.8":np.array(maes_sep),
+        "MAE0.5~2":np.mean(np.array(mae05_2))
+    }
+
+    print("MAE={0:.8f}, MAE0.5~2={1:.8f}".format(summary_data["MAE"],summary_data["MAE0.5~2"]))
+
+    pkl_path = os.path.join("data","kriging_result{}.pickle".format(exi))
+    with open(pkl_path,"wb") as f:
+        pickle.dump(summary_data,f)
+
+
+
+
